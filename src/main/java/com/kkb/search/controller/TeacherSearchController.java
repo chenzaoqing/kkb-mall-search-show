42268520c06aa6845e60b5136604cfe692852352
赵杰 <jzhao@kaikeba.com>
2020-02-24 14:48:49 +0800

    选秀项目

--- src/main/java/com/kkb/search/controller/TeacherSearchController.java
+++ src/main/java/com/kkb/search/controller/TeacherSearchController.java
@@ -3,6 +3,7 @@ package com.kkb.search.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import com.kkb.core.web.KkbResponse;
 import com.kkb.core.web.KkbWebStatus;
 import com.kkb.search.domain.TeacherSearchVo;
@@ -11,8 +12,11 @@ import com.kkb.search.service.ITeacherSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.*;
+
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * <p>
@@ -32,12 +36,19 @@ public class TeacherSearchController {
 	@GetMapping("/{mobile}")
 	public KkbResponse info(@PathVariable String mobile) {
 		List<TeacherSearch> list = iTeacherSearchService.list(new QueryWrapper<TeacherSearch>().lambda().eq(TeacherSearch::getMobile, mobile));
-		return KkbResponse.success(CollectionUtils.isEmpty(list) ? 0 : 1);
+		Map<String, Object> result = Maps.newHashMap();
+		if (CollectionUtils.isEmpty(list)) {
+			result.put("status", 0);
+		} else {
+			result.put("status", 1);
+			result.put("ids",list.stream().map(e -> e.getId()).collect(Collectors.toList()));
+		}
+		return KkbResponse.success(result);
 	}
 
 	@PostMapping("/add")
 	public KkbResponse add(@RequestBody @Valid TeacherSearchVo teacherSearchVo) {
-		if(teacherSearchVo.getTeacherIds().size() < 2 || teacherSearchVo.getTeacherIds().size() > 3){
+		if (teacherSearchVo.getTeacherIds().size() < 2 || teacherSearchVo.getTeacherIds().size() > 3) {
 			return KkbResponse.failure(KkbWebStatus.ILLEGAL_PARAM);
 		}
 		List<TeacherSearch> list = iTeacherSearchService.list(new QueryWrapper<TeacherSearch>().lambda().eq(TeacherSearch::getMobile, teacherSearchVo.getMobile()));
